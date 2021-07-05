994f9c825395aba9374f9b7266f67f859559a302
CJUSI <cjusmith@gmail.com>
2021-07-05 12:21:54 +0100

    001: Added ClassRoster Code Along to github and updated to include a service layer

---
+++ src/main/java/com/sg/classroster/service/ClassRosterServiceLayerImpl.java
@@ -0,0 +1,69 @@
+package com.sg.classroster.service;
+
+import com.sg.classroster.dao.ClassRosterDao;
+import com.sg.classroster.dao.ClassRosterPersistenceException;
+import com.sg.classroster.dto.Student;
+
+import java.util.List;
+
+public class ClassRosterServiceLayerImpl implements ClassRosterServiceLayer{
+
+    ClassRosterDao dao;
+
+    public ClassRosterServiceLayerImpl(ClassRosterDao dao){
+        this.dao = dao;
+    }
+
+    @Override
+    public void createStudent(Student student) throws
+            ClassRosterDuplicateIdException,
+            ClassRosterDataValidationException,
+            ClassRosterPersistenceException {
+        // First check to see if there is already a student w/SID the same
+        // throw a ClassRosterDuplicateIdException
+        if (dao.getStudent(student.getStudentId()) != null) {
+            throw new ClassRosterDuplicateIdException(
+                    "ERROR: Could not create student.  Student Id "
+                            + student.getStudentId()
+                            + " already exists");
+        }
+
+        // Now validate all the fields on the given Student object.
+        validateStudentData(student);
+
+        // We passed all our business rules checks so go ahead and create student object
+        dao.addStudent(student.getStudentId(), student);
+
+    }
+
+    @Override
+    public List<Student> getAllStudents() throws ClassRosterPersistenceException {
+        return dao.getAllStudents();
+    }
+
+    @Override
+    public Student getStudent(String studentId) throws ClassRosterPersistenceException {
+        return dao.getStudent(studentId);
+    }
+
+    @Override
+    public Student removeStudent(String studentId) throws ClassRosterPersistenceException {
+        return dao.removeStudent(studentId);
+    }
+
+    //Check that each field in object is not null and not empty/whitespace
+    private void validateStudentData(Student student) throws
+            ClassRosterDataValidationException {
+
+        if (student.getFirstName() == null
+                || student.getFirstName().trim().length() == 0
+                || student.getLastName() == null
+                || student.getLastName().trim().length() == 0
+                || student.getCohort() == null
+                || student.getCohort().trim().length() == 0) {
+
+            throw new ClassRosterDataValidationException(
+                    "ERROR: All fields [First Name, Last Name, Cohort] are required.");
+        }
+    }
+}
